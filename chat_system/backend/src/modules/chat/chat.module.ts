156fd912032470507778f7c37861014eed168cc7
Matheus R. dos Santos <matheus.ribeiro@sou.inteli.edu.br>
2025-03-22 18:09:20 -0300

    feat: chat with web socket

--- chat_system/backend/src/modules/chat/chat.module.ts
+++ chat_system/backend/src/modules/chat/chat.module.ts
@@ -2,10 +2,11 @@ import { Module } from '@nestjs/common';
 import { DatabaseModule } from 'src/config/database.module';
 import { ChatService } from './chat.service';
 import { ChatController } from './chat.controller';
+import { ChatGateway } from './chat.gateway';
 
 @Module({
     imports: [DatabaseModule],
     controllers: [ChatController],
-    providers: [ChatService],
+    providers: [ChatService, ChatGateway],
 })
 export class ChatModule {}
