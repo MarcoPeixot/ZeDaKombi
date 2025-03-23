42dfe7e1b8649f96d4f489cdf604fe8cbbcf2c59
Matheus R. dos Santos <matheus.ribeiro@sou.inteli.edu.br>
2025-03-23 18:49:47 -0300

    fix: error with deploy

--- chat_system/backend/src/main.ts
+++ chat_system/backend/src/main.ts
@@ -22,7 +22,7 @@ async function bootstrap() {
   SwaggerModule.setup('docs', app, documentFactory);
   // Ativa o filtro de erro globalmente
   app.useGlobalFilters(new GlobalExceptionFilter(mongoDBService));
-  await app.listen(process.env.PORT ?? 3000);
+  await app.listen(process.env.PORT ?? 3000, '0.0.0.0');
   console.log(`Aplicação rodando na porta ${process.env.PORT ?? 3000}`);
 }
 bootstrap();
